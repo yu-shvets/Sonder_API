35b9a6f9ca48bc93427700aca3b8207bd09016dc
Tom <andypandy12345@yahoo.com>
2018-05-03 12:12:00 +0300

    Update.

--- Sonder_API/settings.py
+++ Sonder_API/settings.py
@@ -149,7 +149,7 @@ MEDIA_ROOT = '/home/django/Sonder_API/media/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.AllowAny'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
